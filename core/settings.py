14bc9ef27f9dfbb02dba1577bf09769bd99babc6
phanstudio <donenoelle@gmail.com>
2025-12-10 13:13:15 +0100

    added static dirs

--- core/settings.py
+++ core/settings.py
@@ -161,6 +161,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
