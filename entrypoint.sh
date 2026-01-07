7750ae426a05bcc724b034ecd7f387b909d733ea
phanstudio <donenoelle@gmail.com>
2026-01-07 20:07:51 +0100

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,14 +8,14 @@ echo "Running migrations..."
 # python manage.py migrate accounts --noinput
 # python manage.py migrate accounts 0026 --fake --noinput
 
-# python3 manage.py makemigrations --noinput
-# python3 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 # python manage.py migrate accounts 0012 --fake
 # python manage.py migrate
 # python manage.py showmigrations accounts
 
 echo "Collecting static files..."
-# python3 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 echo "Starting server..."
 exec "$@"
