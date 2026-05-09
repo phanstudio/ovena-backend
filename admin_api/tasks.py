6ec9dd81d40a4534a57a9a06c14f89d3807ca358
phanstudio <85735876+phanstudio@users.noreply.github.com>
2026-05-09 11:48:16 +0100

    initing the chage on the task for admins

---
+++ admin_api/tasks.py
@@ -0,0 +1,10 @@
+# from celery import shared_task
+# import logging
+# logger = logging.getLogger(__name__)
+
+# # we need for buisnesses there admins we need for drivers
+# # @shared_task(name='orders.check_if_buissness_passed_all_checks')
+# # def check_if_buissness_passed_all_checks(order_id):
+# #     ...
+
+# at the end if all is well set is_approved to true
