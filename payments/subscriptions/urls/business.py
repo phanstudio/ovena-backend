fba97d3ea11dd3326df175596a47dfb5fa5a18a0
phanstudio <85735876+phanstudio@users.noreply.github.com>
2026-06-02 09:06:49 +0100

    update

--- payments/subscriptions/urls/business.py
+++ payments/subscriptions/urls/business.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .. import views
+
 prefix = "business-"
-urlpatterns = [ # /client
+urlpatterns = [
     path("subscription/", views.BusinessCreateSubscriptionView.as_view(), name=prefix+"subscription-create"),
     path("subscription/cancle/", views.BusinessCancelSubscriptionView.as_view(), name=prefix+"subscription-cancle"),
     path("subscription/current/", views.BusinessCurrentSubscriptionView.as_view(), name=prefix+"subscription-current"),
     path("invoice/history/", views.BusinessInvoiceHistoryView.as_view(), name=prefix+"invoice-history"),
-    path("invoice/retry/<int:invoice_id>", views.BusinessRetryInvoicePaymentView.as_view(), name=prefix+"invoice-retry"),
+    path("invoice/retry/<int:invoice_id>/", views.BusinessRetryInvoicePaymentView.as_view(), name=prefix+"invoice-retry"),
     path("update/card/link/", views.BusinessSubscriptionUpdateCardView.as_view(), name=prefix+"update-card"),
     path("plans/list/", views.BusinessPlanListView.as_view(), name=prefix+"plan-list"),
 ]
