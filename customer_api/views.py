c0220406632fb0238c2368297460c74236dc4a0e
phanstudio <85735876+phanstudio@users.noreply.github.com>
2026-06-10 09:36:42 +0100

    update

--- customer_api/views.py
+++ customer_api/views.py
@@ -164,7 +164,7 @@ class OrderCalculationsView(BaseCustomerAPIView):
         user_location = make_point(vd["long"], vd["lat"])
         
         vd["branch_id"]# or branch long and lat;
-        branch = Branch.objects.filter(id=vd["branch_id"], is_active=True)
+        branch = Branch.objects.filter(id=vd["branch_id"], is_active=True).first()
         if not branch:
             return Response({"details": "branch id invalid or not active"}, status=401)
         
